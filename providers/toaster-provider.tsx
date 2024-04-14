b0a89df59e62c605deac9917fcef32ee0850bb6a
Alish <alishbaig2004@gmail.com>
2024-04-15 05:19:39 +0530

    feat: Update ToasterProvider styles to match with the theme

--- providers/toaster-provider.tsx
+++ providers/toaster-provider.tsx
@@ -8,6 +8,13 @@ export const ToasterProvider = () => {
     <Toaster
       toastOptions={{
         position: "bottom-right",
+        style: {
+          background: "oklch(var(--n))",
+          color: "oklch(var(--nc))",
+          fontSize: "1rem",
+          padding: "1rem",
+          borderRadius: "0.6rem",
+        },
       }}
     />
   );
