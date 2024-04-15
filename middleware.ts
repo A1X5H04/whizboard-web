e8ac35c220e2cfc54706067f6a2e4e5e4e8c1c9d
Alish <alishbaig2004@gmail.com>
2024-04-15 06:47:03 +0530

    refactor: disabled clerk's debug logs

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({
-  debug: true,
-});
+export default authMiddleware();
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
