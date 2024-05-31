0d9ac1b2a3f553af667a4aea508c949b475f2167
Alish <alishbaig2004@gmail.com>
2024-05-31 22:53:39 +0530

    style: update loading component with loading dots

--- app/(dashboard)/_components/emptystates/loading.tsx
+++ app/(dashboard)/_components/emptystates/loading.tsx
@@ -4,7 +4,7 @@ function Loading() {
   return (
     <div className="grid place-items-center h-full">
       <div className="text-center flex flex-col items-center justify-center">
-        <span className="loading loading-dots loading-md"></span>
+        <span className="loading loading-dots loading-md" />
         <p>Loading Boards</p>
       </div>
     </div>
