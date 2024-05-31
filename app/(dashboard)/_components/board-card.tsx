7e14dc7c073156a14cc67081744075623df3d914
Alish <alishbaig2004@gmail.com>
2024-05-31 22:48:44 +0530

    style: improved hierarchy and add icon for upcoming feature!

--- app/(dashboard)/_components/board-card.tsx
+++ app/(dashboard)/_components/board-card.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useAuth } from "@clerk/nextjs";
-import { Star } from "@phosphor-icons/react";
+import { GlobeSimple, LockSimple, Star } from "@phosphor-icons/react";
 import Link from "next/link";
 import Image from "next/image";
 import { formatTimestamp } from "@/libs/format";
@@ -33,6 +33,7 @@ function BoardCard({
   isFavourite,
 }: BoardCardProps) {
   const { userId } = useAuth();
+  const isPublic = false;
   const favourite = useMutation(api.board.favourite);
   const unfavourite = useMutation(api.board.unfavourite);
   const authorLabel = authorId === userId ? "You" : authorName;
@@ -57,21 +58,36 @@ function BoardCard({
   return (
     <div>
       <Link href={`/board/${id}`}>
-        <div className="group w-full h-80 rounded-box flex flex-col justify-between shadow-xl bg-base-100">
-          <div className="relative flex-1 m-1 rounded-box rounded-b-xl overflow-hidden">
+        <div className="group w-full h-80 rounded-box flex flex-col justify-between shadow-md bg-base-200 p-1.5">
+          <div className="relative flex-1 rounded-box overflow-hidden">
             <Image src={imageUrl} alt={title} fill className="object-cover" />
             {/* <div className="opacity-0 group-hover:opacity-75 transition-opacity h-full w-full bg-base-300" /> */}
             <div className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity">
               <BoardActions boardId={id} />
             </div>
           </div>
-          <div className="px-3 pb-3 pt-1">
+          <div className="p-2">
             <div className="flex items-center justify-between">
-              <h2 className="text-base font-semibold truncate">{title}</h2>
+              <span className="inline-flex items-center gap-x-1">
+                <h2 className="text-lg font-semibold truncate">{title}</h2>
+                <div
+                  className="lg:tooltip"
+                  data-tip={isPublic ? "Public" : "Private"}
+                >
+                  {isPublic ? (
+                    <GlobeSimple
+                      className="fill-current opacity-75"
+                      size={15}
+                    />
+                  ) : (
+                    <LockSimple className="fill-current opacity-75" size={15} />
+                  )}
+                </div>
+              </span>
               <Star
                 onClick={toggleFavourite}
                 weight={isFavourite ? "fill" : "regular"}
-                className="fill-secondary hover:opacity-60 transition-opacity cursor-pointer"
+                className="fill-secondary hover:opacity-60 transition-opacity cursor-pointer "
                 size={20}
               />
             </div>
