a94a069336d60690e1da5554452cdf732bcf1276
Alish <alishbaig2004@gmail.com>
2024-05-31 22:51:37 +0530

    feat: add theme toggler and removed unused imports

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,19 +1,17 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
-import { CreateOrganization } from "@clerk/nextjs";
 import React from "react";
 import SearchInput from "./search-input";
-import { useRouter } from "next/navigation";
+import ThemeToggler from "@/components/theme-toggler";
 
 function Navbar() {
-  const router = useRouter();
-
   return (
     <div className="flex items-center gap-x-4 p-5">
       <div className="hidden lg:flex lg:flex-1">
         <SearchInput />
       </div>
+      <ThemeToggler />
       <UserButton afterSignOutUrl="/" />
     </div>
   );
