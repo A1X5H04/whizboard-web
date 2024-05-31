9d66da60edf8e8a591bc612a292a30d92427a373
Alish <alishbaig2004@gmail.com>
2024-05-31 22:54:50 +0530

    refactor: update org-modal and create organization button with modal functionality

--- app/(dashboard)/_components/sidebar/neworg-btn.tsx
+++ app/(dashboard)/_components/sidebar/neworg-btn.tsx
@@ -3,38 +3,19 @@
 import React from "react";
 import { CreateOrganization } from "@clerk/nextjs";
 import { Plus } from "@phosphor-icons/react";
+import OrgModal from "../org-modal";
 
 function NewOrgButton() {
+  const modalRef = React.useRef<HTMLDialogElement>(null);
   return (
     <div className="tooltip tooltip-right" data-tip="Create Organization">
       <button
         className="btn-square btn-sm btn"
-        onClick={() =>
-          (
-            document?.getElementById("org_modal") as HTMLDialogElement
-          )?.showModal()
-        }
+        onClick={() => modalRef.current?.showModal()}
       >
         <Plus size={18} />
       </button>
-      <dialog id="org_modal" className="modal bg-transparent">
-        <div className="modal-box bg-inherit shadow-none overflow-visible">
-          <CreateOrganization
-            appearance={{
-              elements: {
-                rootBox: {
-                  backgroundColor: "bg-slate-700",
-                  borderRadius: "rounded-md",
-                  padding: "p-5",
-                },
-              },
-            }}
-          />
-        </div>
-        <form method="dialog" className="modal-backdrop">
-          <button>close</button>
-        </form>
-      </dialog>
+      <OrgModal modalRef={modalRef} />
     </div>
   );
 }
